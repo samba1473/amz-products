ddc55950d827fec1bac06361c464a9ef02cf71f7
samba1473 <sambasivarao1473@gmail.com>
2022-12-19 00:08:22 +0530

    message

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { ProductService } from '../product.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'] 
 })
 export class HomeComponent implements OnInit,AfterViewInit {
   totalSelectedItems:any;
